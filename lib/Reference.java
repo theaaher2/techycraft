7eab07f04add4aac8165773c9e20050207fe93ca
adam ahern <adambruzzz@rocketmail.com>
2013-05-03 09:35:59 +1000

    Base classes

---
+++ lib/Reference.java
@@ -0,0 +1,12 @@
+package TechCraft.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "TC";
+	public static final String MOD_NAME = "Tech Craft";
+	public static final String VERSION = "0.0.1";
+			
+	
+	
+	
+}
