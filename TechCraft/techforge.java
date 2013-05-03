00e67b019d4681d7687950a5ec31b460d5e0a1a6
adam ahern <adambruzzz@rocketmail.com>
2013-05-03 10:36:01 +1000

    Setup Basic coding area

---
+++ TechCraft/techforge.java
@@ -0,0 +1,35 @@
+package TechCraft.TechCraft;
+
+
+
+import TechCraft.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID , name = Reference.MOD_NAME, version = Reference.VERSION)
+
+public class techforge{
+
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+   
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ lib/Reference.java
@@ -0,0 +1,12 @@
+package TechCraft.lib;
+
+
+public class Reference {
+
+   
+    public static final String MOD_ID = "Tech forge";
+    
+    public static final String MOD_NAME = "TechForge";
+  
+    public static final String VERSION = "0.0.1";
+}
